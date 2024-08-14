b1052e49c741a11f7d85e212a5526585ec5d5df2
Sanskar Dahiya <sanskardahiya98@gmail.com>
2024-08-14 16:49:46 +0530

    added some crazzy stuff to blow internet

--- app/(root_page)/layout.tsx
+++ app/(root_page)/layout.tsx
@@ -1,7 +1,87 @@
-import Script from 'next/script'
-import '@/styles/global.scss'
-import '@/app/globals.css'
 import NavbarSection from '@/components/NavbarSection'
+import Script from 'next/script'
+
+const PAGE_NAME = 'Sanskar Dahiya'
+const PAGE_DESCRIPTION =
+  "Welcome to Sanskar Dahiya's portfolio website a full-stack web developer with a focus on JavaScript, Next.js. I has the skills and experience to help bring your web development projects to life."
+// 'Full Stack Web Developer | Experience in NextJS, ReactJS | Software Developer Engineer'
+const jsonLd = {
+  '@context': 'https://schema.org',
+  '@type': 'Person',
+  name: PAGE_NAME,
+  additionalName: 'Sanskar',
+  address: {
+    '@type': 'PostalAddress',
+    addressCountry: 'India',
+    addressRegion: 'Haryana',
+    addressLocality: 'Haryana',
+    streetAddress: 'Haryana',
+  },
+  // url: 'https://www.yourwebsite.com',
+  // image: 'https://www.yourwebsite.com/images/yourimage.jpg',
+  description: PAGE_DESCRIPTION,
+  sameAs: [
+    'https://www.linkedin.com/in/sanskardahiya/',
+    // 'https://twitter.com/yourtwitterhandle',
+    'https://www.instagram.com/sanskar__dahiya/',
+  ],
+  email: 'mailto:sanskardahiya98@gmail.com',
+  jobTitle: 'Software Developer Engineer',
+  headline: 'Portfolio Website | Sanskar Dahiya',
+
+  worksFor: {
+    '@type': 'Organization',
+    name: 'Loco',
+    url: 'https://loco.gg/',
+  },
+  alumniOf: {
+    '@type': 'CollegeOrUniversity',
+    name: 'Seth Jai Parkash Mukand Lal Institute of Engineering and Technology',
+    url: 'https://www.jmit.ac.in/',
+  },
+  // hasOccupation: [
+  //   {
+  //     '@type': 'JobPosting',
+  //     title: 'Software Developer Engineer',
+  //     employmentType: 'Full-time',
+  //     hiringOrganization: {
+  //       '@type': 'Organization',
+  //       name: 'Daffodil Software',
+  //       url: 'https://www.daffodilsw.com/',
+  //     },
+  //     jobLocation: {
+  //       '@type': 'Place',
+  //       address: {
+  //         '@type': 'PostalAddress',
+  //         addressLocality: 'Hisar',
+  //         addressRegion: 'Haryana',
+  //         addressCountry: 'India',
+  //       },
+  //     },
+  //   },
+  // ],
+  knowsLanguage: [
+    {
+      '@type': 'Language',
+      name: 'English',
+      alternateName: 'en',
+    },
+    {
+      '@type': 'Language',
+      name: 'Hindi',
+      alternateName: 'hi',
+    },
+  ],
+  datePublished: new Date(),
+}
+export const metadata = {
+  ...jsonLd,
+  title: jsonLd.name,
+  openGraph: {
+    ...jsonLd,
+    title: jsonLd.name,
+  },
+}
 
 export default function RootLayout({
   children,
@@ -9,11 +89,20 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className="scrollbar-hide relative">
+    <>
+      <head>
+        <Script
+          id="myData"
+          type="application/ld+json"
+          suppressHydrationWarning={true}
+        >
+          {JSON.stringify(jsonLd)}
+        </Script>
+      </head>
+      <body>
         {children}
         <NavbarSection />
       </body>
-    </html>
+    </>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,88 +1,5 @@
-import Script from 'next/script'
 import '@/styles/global.scss'
 import '@/app/globals.css'
-
-const PAGE_NAME = 'Sanskar Dahiya'
-const PAGE_DESCRIPTION =
-  "Welcome to Sanskar Dahiya's portfolio website a full-stack web developer with a focus on JavaScript, Next.js. I has the skills and experience to help bring your web development projects to life."
-// 'Full Stack Web Developer | Experience in NextJS, ReactJS | Software Developer Engineer'
-const jsonLd = {
-  '@context': 'https://schema.org',
-  '@type': 'Person',
-  name: PAGE_NAME,
-  additionalName: 'Sanskar',
-  address: {
-    '@type': 'PostalAddress',
-    addressCountry: 'India',
-    addressRegion: 'Haryana',
-    addressLocality: 'Haryana',
-    streetAddress: 'Haryana',
-  },
-  // url: 'https://www.yourwebsite.com',
-  // image: 'https://www.yourwebsite.com/images/yourimage.jpg',
-  description: PAGE_DESCRIPTION,
-  sameAs: [
-    'https://www.linkedin.com/in/sanskardahiya/',
-    // 'https://twitter.com/yourtwitterhandle',
-    'https://www.instagram.com/sanskar__dahiya/',
-  ],
-  email: 'mailto:sanskardahiya98@gmail.com',
-  jobTitle: 'Software Developer Engineer',
-  headline: 'Portfolio Website | Sanskar Dahiya',
-
-  worksFor: {
-    '@type': 'Organization',
-    name: 'Loco',
-    url: 'https://loco.gg/',
-  },
-  alumniOf: {
-    '@type': 'CollegeOrUniversity',
-    name: 'Seth Jai Parkash Mukand Lal Institute of Engineering and Technology',
-    url: 'https://www.jmit.ac.in/',
-  },
-  // hasOccupation: [
-  //   {
-  //     '@type': 'JobPosting',
-  //     title: 'Software Developer Engineer',
-  //     employmentType: 'Full-time',
-  //     hiringOrganization: {
-  //       '@type': 'Organization',
-  //       name: 'Daffodil Software',
-  //       url: 'https://www.daffodilsw.com/',
-  //     },
-  //     jobLocation: {
-  //       '@type': 'Place',
-  //       address: {
-  //         '@type': 'PostalAddress',
-  //         addressLocality: 'Hisar',
-  //         addressRegion: 'Haryana',
-  //         addressCountry: 'India',
-  //       },
-  //     },
-  //   },
-  // ],
-  knowsLanguage: [
-    {
-      '@type': 'Language',
-      name: 'English',
-      alternateName: 'en',
-    },
-    {
-      '@type': 'Language',
-      name: 'Hindi',
-      alternateName: 'hi',
-    },
-  ],
-  datePublished: new Date(),
-}
-export const metadata = {
-  ...jsonLd,
-  title: jsonLd.name,
-  openGraph: {
-    ...jsonLd,
-    title: jsonLd.name,
-  },
-}
 
 export default function RootLayout({
   children,
@@ -96,15 +13,8 @@ export default function RootLayout({
           name="google-site-verification"
           content="9aPGR4HnQhgMxCt1Ts0HrGKulzq-i1WxiQixlBYya9s"
         />
-        <Script
-          id="myData"
-          type="application/ld+json"
-          suppressHydrationWarning={true}
-        >
-          {JSON.stringify(jsonLd)}
-        </Script>
       </head>
-      <body>{children}</body>
+      <body className="scrollbar-hide relative">{children}</body>
     </html>
   )
 }
